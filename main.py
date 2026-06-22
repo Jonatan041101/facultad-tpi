ae292a6af98ca1de1b48f4d1b9472f49d4ce6a5f
Jonatan Valdiviezo <valdiviezojonatan.2101@gmail.com>
2026-06-22 20:12:51 -0300

    feat: agrega el run principal

---
+++ main.py
@@ -0,0 +1,12 @@
+"""Punto de entrada de la aplicación."""
+
+from countries_menu import run_menu
+
+
+def main():
+    """Inicia el menú principal."""
+    run_menu()
+
+
+if __name__ == "__main__":
+    main()
